1b0a9f7d815bb061012d14fe1f3758125b6e5ca5
faisalrehman <faisalfasi18@gmail.com>
2023-09-15 15:30:10 +0200

    deploy test

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,7 +4,7 @@ import "./Home.scss";
 import FeaturedProducts from "../../components/FeaturedProducts/FeaturedProducts";
 import Categories from "../../components/Categories/Categories";
 import Contact from "../../components/Contact/Contact";
-
+//testing
 const Home = () => {
   return (
     <div className="home">
